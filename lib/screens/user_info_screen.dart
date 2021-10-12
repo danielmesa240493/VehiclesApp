b9c1940ee88e5332bde62e7877869da43e1522d0
danielmesa240493 <danielmesa240493@correo.itm.edu.co>
2021-10-12 17:23:00 -0500

    VehiclesCaruselPhoto

--- lib/screens/user_info_screen.dart
+++ lib/screens/user_info_screen.dart
@@ -1,9 +1,18 @@
+import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:flutter/material.dart';
 
 import 'package:vehicles_app/components/loader_component.dart';
+import 'package:vehicles_app/helpers/api_helper.dart';
+import 'package:vehicles_app/models/brand.dart';
+import 'package:vehicles_app/models/response.dart';
 import 'package:vehicles_app/models/token.dart';
 import 'package:vehicles_app/models/user.dart';
+import 'package:vehicles_app/models/vehicle.dart';
+import 'package:vehicles_app/models/vehicle_type.dart';
 import 'package:vehicles_app/screens/user_screen.dart';
+import 'package:vehicles_app/screens/vehicle_screen.dart';
 
 class UserInfoScreen extends StatefulWidget {
   final Token token;
@@ -20,21 +29,40 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
   late User _user;
 
   @override
+  void initState() {
+    super.initState();
+    _user = widget.user;
+    _getUser();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.user.fullName),
+        title: Text(_user.fullName),
       ),
-      body: Stack(
-        children: <Widget>[
-          Column(
-            children: <Widget>[
-              _showUserInfo(),
-              _showButtons(),
-            ],
-          ),
-          _showLoader ? LoaderComponent(text: 'Por favor espere...',) : Container(),
-        ],
+      body: Center(
+        child: _showLoader 
+          ? LoaderComponent(text: 'Por favor espere...',) 
+          : _getContent(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _goVehicle(Vehicle(
+          brand: Brand(id: 0, description: ''), 
+          color: '', 
+          histories: [], 
+          historiesCount: 0, 
+          id: 0, 
+          imageFullPath: '', 
+          line: '', 
+          model: 2021, 
+          plaque: '', 
+          remarks: '', 
+          vehiclePhotos: [], 
+          vehiclePhotosCount: 0, 
+          vehicleType: VehicleType(id: 0, description: '')
+        )),
       ),
     );
   }
@@ -49,12 +77,18 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
             children: <Widget>[
               ClipRRect(
                 borderRadius: BorderRadius.circular(50),
-                child: FadeInImage(
-                  placeholder: AssetImage('assets/vehicles_logo.png'), 
-                  image: NetworkImage(_user.imageFullPath),
-                  width: 100,
+                child: CachedNetworkImage(
+                  imageUrl: _user.imageFullPath,
+                  errorWidget: (context, url, error) => Icon(Icons.error),
+                  fit: BoxFit.cover,
                   height: 100,
-                  fit: BoxFit.cover
+                  width: 100,
+                  placeholder: (context, url) => Image(
+                    image: AssetImage('assets/vehicles_logo.png'),
+                    fit: BoxFit.cover,
+                    height: 100,
+                    width: 100,
+                  ),
                 ),
               ),
               Positioned(
@@ -202,12 +236,6 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
     );
   }
 
-  Widget _showButtons() {
-    return Container(
-      
-    );
-  }
-
   void _goEdit() async {
     String? result = await Navigator.push(
       context, 
@@ -221,5 +249,183 @@ class _UserInfoScreenState extends State<UserInfoScreen> {
     if (result == 'yes') {
       //TODO: Pending refresh user info
     }
+  }
+
+  Future<Null> _getUser() async {
+    setState(() {
+      _showLoader = true;
+    });
+
+    var connectivityResult = await Connectivity().checkConnectivity();
+    if (connectivityResult == ConnectivityResult.none) {
+      setState(() {
+        _showLoader = false;
+      });
+      await showAlertDialog(
+        context: context,
+        title: 'Error', 
+        message: 'Verifica que estes conectado a internet.',
+        actions: <AlertDialogAction>[
+            AlertDialogAction(key: null, label: 'Aceptar'),
+        ]
+      );    
+      return;
+    }
+
+    Response response = await ApiHelper.getUser(widget.token, _user.id);
+
+    setState(() {
+      _showLoader = false;
+    });
+
+    if (!response.isSuccess) {
+      await showAlertDialog(
+        context: context,
+        title: 'Error', 
+        message: response.message,
+        actions: <AlertDialogAction>[
+            AlertDialogAction(key: null, label: 'Aceptar'),
+        ]
+      );    
+      return;
+    }
+
+    setState(() {
+      _user = response.result;
+    });
+  }
+
+  void _goVehicle(Vehicle vehicle) async { 
+    String? result = await  Navigator.push(
+      context, 
+      MaterialPageRoute(
+        builder: (context) => VehicleScreen(
+          token: widget.token, 
+          user: _user, 
+          vehicle: vehicle
+        ) 
+      )
+    );
+    if (result == 'yes') {
+      _getUser();
+    }
+  }
+
+  Widget _getContent() {
+    return Column(
+      children: <Widget>[
+        _showUserInfo(),
+        Expanded(
+          child: _user.vehicles.length == 0 ? _noContent() : _getListView(),
+        ),
+      ],
+    );
+  }
+
+  Widget _getListView() {
+    return RefreshIndicator(
+      onRefresh: _getUser,
+      child: ListView(
+        children: _user.vehicles.map((e) {
+          return Card(
+            child: InkWell(
+              onTap: () => _goVehicle(e),
+              child: Container(
+                margin: EdgeInsets.all(10),
+                padding: EdgeInsets.all(5),
+                child: Row(
+                  children: <Widget>[
+                    CachedNetworkImage(
+                      imageUrl: e.imageFullPath,
+                      errorWidget: (context, url, error) => Icon(Icons.error),
+                      fit: BoxFit.cover,
+                      height: 80,
+                      width: 80,
+                      placeholder: (context, url) => Image(
+                        image: AssetImage('assets/vehicles_logo.png'),
+                        fit: BoxFit.cover,
+                        height: 80,
+                        width: 80,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: <Widget>[
+                            Column(
+                              children: <Widget>[
+                                Text(
+                                  e.plaque,
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold
+                                  ),
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      e.vehicleType.description,
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    SizedBox(width: 5,),
+                                    Text(
+                                      e.brand.description,
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: [
+                                    Text(
+                                      e.line,
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                    SizedBox(width: 5,),
+                                    Text(
+                                      e.color,
+                                      style: TextStyle(
+                                        fontSize: 14,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ),
+                    Icon(Icons.arrow_forward_ios, size: 40,)
+                  ],
+                ),
+              ),
+            ),
+          );
+        }).toList(),
+      ), 
+    );
+  }
+
+  Widget _noContent() {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.all(20),
+        child: Text(
+          'El usuario no tiene vehículos registrados.',
+          style: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
